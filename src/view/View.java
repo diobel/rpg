58f65ab14101186ea2eb7aa6e01bc2aef38bd019
diobel <diabel66621@gmail.com>
2015-11-03 00:29:23 +0100

    tak jak było

--- src/view/View.java
+++ src/view/View.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * View class that is responsible for interaction with user
  *
  * @author Michał Szubert
- * @version 1.01
+ * @version 1.0
  *
  */
 public class View {
